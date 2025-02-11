d61dd15387617d6b2c53164f48c32c6524e9355c
Liam Vanderpoel <liam@superlinear.com>
2025-02-11 16:13:09 +0100

    Update DevContainer post-create script dependencies

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 set -e
 
+export UV_LINK_MODE=copy
 uv venv openr1 --seed --python 3.11
 source openr1/bin/activate
 
-uv pip install --upgrade pip --link-mode=copy
-uv pip install vllm==0.7.1 --link-mode=copy
-FLASH_ATTENTION_SKIP_CUDA_BUILD=TRUE  uv pip install flash-attn --no-build-isolation --link-mode=copy
+uv pip install --upgrade pip
+uv pip install wandb
+uv pip install vllm==0.7.1
+FLASH_ATTENTION_SKIP_CUDA_BUILD=TRUE  uv pip install flash-attn --no-build-isolation
 
 # Install project in development mode
-GIT_LFS_SKIP_SMUDGE=1 uv pip install -e '.[dev]' --no-build-isolation --link-mode=copy 
+GIT_LFS_SKIP_SMUDGE=1 uv pip install -e '.[dev]' --no-build-isolation
